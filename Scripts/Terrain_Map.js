279f5c94e72407e9082840edb8f40f043d33c5ab
ayushdad22 <156011784+ayushdad22@users.noreply.github.com>
2024-10-06 22:01:23 +0100

    Added Link

--- Scripts/Terrain_Map.js
+++ Scripts/Terrain_Map.js
@@ -87,7 +87,7 @@ const customLayer = {
 
    
     const loader = new THREE.GLTFLoader();
-    loader.load("../../plane.gltf", (gltf) => {
+    loader.load("/Nasa-Hackathon-Uncle-Bao-/plane.gltf", (gltf) => {
       this.model = gltf.scene;
       const modelScaleFactor = 1000; 
       this.model.scale.set(
