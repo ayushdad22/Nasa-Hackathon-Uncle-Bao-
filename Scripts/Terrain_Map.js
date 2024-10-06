9a5eede2f88d3ba023d0b786f8b0169d22e1a766
ayushdad22 <156011784+ayushdad22@users.noreply.github.com>
2024-10-06 21:27:56 +0100

    Co-authored-by: baileyluTCD <baileyluTCD@users.noreply.github.com>

--- Scripts/Terrain_Map.js
+++ Scripts/Terrain_Map.js
@@ -1,32 +1,32 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXl1c2hkYWQiLCJhIjoiY20xdGI4YTdhMDBvODJqcGJqaDV3bzQ1MCJ9.djla3giXDBGAh6Ked3QBeg';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXl1c2hkYWQiLCJhIjoiY20xdGI4YTdhMDBvODJqcGJqaDV3bzQ1MCJ9.djla3giXDBGAh6Ked3QBeg";
 
 // Initialize Mapbox map
 const map = new mapboxgl.Map({
-  container: 'map',
-  zoom: 11  ,
-  center: [-9.310, 53.957],
+  container: "map",
+  zoom: 11,
+  center: [-9.31, 53.957],
   pitch: 120,
   bearing: -177.2,
-  style: 'mapbox://styles/mapbox/satellite-v9',
+  style: "mapbox://styles/mapbox/satellite-v9",
   antialias: true,
-  interactive: false
+  interactive: false,
 });
 
-map.on('style.load', () => {
+map.on("style.load", () => {
   // map.setFog({}); // Optional: Adds atmospheric fog for depth
-  map.addSource('mapbox-dem', {
-    'type': 'raster-dem',
-    'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-    'tileSize': 512,
-    'maxzoom': 14
+  map.addSource("mapbox-dem", {
+    type: "raster-dem",
+    url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    tileSize: 512,
+    maxzoom: 14,
   });
 
-  map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+  map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
 });
 
-
 // model coordinates
-const modelOrigin = [-9.310, 53.95];
+const modelOrigin = [-9.31, 53.95];
 const modelAltitude = 1500;
 const modelRotate = [Math.PI / 2, 0, 0];
 
@@ -42,35 +42,40 @@ const modelTransform = {
   rotateX: modelRotate[0],
   rotateY: modelRotate[1],
   rotateZ: modelRotate[2],
-  scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+  scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
 };
 
 const THREE = window.THREE;
 
 const customLayer = {
-  id: '3d-model',
-  type: 'custom',
-  renderingMode: '3d',
+  id: "3d-model",
+  type: "custom",
+  renderingMode: "3d",
   onAdd: function (map, gl) {
     this.camera = new THREE.Camera();
     this.scene = new THREE.Scene();
     const skybox_loader = new THREE.CubeTextureLoader();
     const skyboxImages = [
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg', // Right
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg', // Left
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg', // Top
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg', // Bottom
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg', // Front
-      '/Night_Sky_Mayo_Resized_1024x1024.jpg'  // Back
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Right
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Left
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Top
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Bottom
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Front
+      "/Night_Sky_Mayo_Resized_1024x1024.jpg", // Back
     ];
 
-    const skyboxTexture = skybox_loader.load(skyboxImages, () => {
-      console.log("Skybox loaded successfully.");
-    }, undefined, (err) => {
-      console.error("An error occurred loading the skybox", err);
-    });
+    const skyboxTexture = skybox_loader.load(
+      skyboxImages,
+      () => {
+        console.log("Skybox loaded successfully.");
+      },
+      undefined,
+      (err) => {
+        console.error("An error occurred loading the skybox", err);
+      }
+    );
     this.scene.background = skyboxTexture;
-    console.log(this.scene.background );
+    console.log(this.scene.background);
     // create two three.js lights to illuminate the model
     const directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(0, 70, 100).normalize();
@@ -82,10 +87,14 @@ const customLayer = {
 
     // Add the 3D model to the three.js scene
     const loader = new THREE.GLTFLoader();
-    loader.load('/plane.gltf', (gltf) => {
+    loader.load("/plane.gltf", (gltf) => {
       this.model = gltf.scene;
-      const modelScaleFactor = 1000;  // Adjust this to make the model larger or smaller
-      this.model.scale.set(modelScaleFactor, modelScaleFactor, modelScaleFactor);
+      const modelScaleFactor = 1000; // Adjust this to make the model larger or smaller
+      this.model.scale.set(
+        modelScaleFactor,
+        modelScaleFactor,
+        modelScaleFactor
+      );
       this.scene.add(this.model);
     });
     this.map = map;
@@ -94,7 +103,7 @@ const customLayer = {
     this.renderer = new THREE.WebGLRenderer({
       canvas: map.getCanvas(),
       context: gl,
-      antialias: true
+      antialias: true,
     });
 
     this.renderer.autoClear = false;
@@ -135,68 +144,67 @@ const customLayer = {
     this.renderer.resetState();
     this.renderer.render(this.scene, this.camera);
     this.map.triggerRepaint();
-  }
+  },
 };
-document.getElementById('goToMayo').addEventListener('click', () => {
+document.getElementById("goToMayo").addEventListener("click", () => {
   map.flyTo({
-      center: [-9.310, 53.957],
-      zoom: 11,
-      pitch: 120,
-      bearing: -177.2,
-      speed: 1.5,
-      curve: 1,
-      essential: true
+    center: [-9.31, 53.957],
+    zoom: 11,
+    pitch: 120,
+    bearing: -177.2,
+    speed: 1.5,
+    curve: 1,
+    essential: true,
   });
-  const dublinCoordinates = [-9.310, 53.957]
+  const dublinCoordinates = [-9.31, 53.957];
 
   const dublinAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
     dublinCoordinates,
-    modelAltitude  
+    modelAltitude
   );
 
   modelTransform.translateX = dublinAsMercatorCoordinate.x;
   modelTransform.translateY = dublinAsMercatorCoordinate.y;
-  modelTransform.translateZ = dublinAsMercatorCoordinate.z; 
-  camera.position.x =dublinAsMercatorCoordinate.x;
-  camera.position.y =dublinAsMercatorCoordinate.y - 0.001;
+  modelTransform.translateZ = dublinAsMercatorCoordinate.z;
+  camera.position.x = dublinAsMercatorCoordinate.x;
+  camera.position.y = dublinAsMercatorCoordinate.y - 0.001;
   //camera.position.z =dublinAsMercatorCoordinate.z;
 });
 
-document.getElementById('goToDublin').addEventListener('click', () => {
+document.getElementById("goToDublin").addEventListener("click", () => {
   map.flyTo({
-      center: [-6.2603, 53.3498], // Dublin coordinates
-      zoom: 11,
-      pitch: 120,
-      bearing: -177.2,
-      speed: 1.5,
-      curve: 1,
-      essential: true
+    center: [-6.2603, 53.3498], // Dublin coordinates
+    zoom: 11,
+    pitch: 120,
+    bearing: -177.2,
+    speed: 1.5,
+    curve: 1,
+    essential: true,
   });
   const dublinCoordinates = [-6.2603, 53.3498];
 
-// Convert the geographic coordinates to Mercator coordinates
-const dublinAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
-  dublinCoordinates,
-  modelAltitude  // Use the same altitude you want for the model
-);
+  
+  const dublinAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
+    dublinCoordinates,
+    modelAltitude 
+  );
 
-// Now, update the model transform's translateX, translateY, and translateZ
-modelTransform.translateX = dublinAsMercatorCoordinate.x;
-modelTransform.translateY = dublinAsMercatorCoordinate.y;
-modelTransform.translateZ = dublinAsMercatorCoordinate.z; 
-camera.position.x =dublinAsMercatorCoordinate.x;
-  camera.position.y =dublinAsMercatorCoordinate.y - 0.001;
- // camera.position.z =dublinAsMercatorCoordinate.z;
-
+  // Now, update the model transform's translateX, translateY, and translateZ
+  modelTransform.translateX = dublinAsMercatorCoordinate.x;
+  modelTransform.translateY = dublinAsMercatorCoordinate.y;
+  modelTransform.translateZ = dublinAsMercatorCoordinate.z;
+  camera.position.x = dublinAsMercatorCoordinate.x;
+  camera.position.y = dublinAsMercatorCoordinate.y - 0.001;
+  // camera.position.z =dublinAsMercatorCoordinate.z;
 });
-map.on('style.load', () => {
+map.on("style.load", () => {
   map.addLayer(customLayer);
 });
 
 const movementSpeed = 500;
 const camera = map.getFreeCameraOptions();
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener("keydown", function (event) {
   const speed = 0.0001;
 
   switch (event.key) {
@@ -204,7 +212,7 @@ document.addEventListener('keydown', function (event) {
       camera.position.y += speed;
       modelTransform.rotateY = 0;
       modelTransform.translateY += speed;
-      
+
       break;
     case "a": // Move left
       camera.position.x += speed;
@@ -218,15 +226,13 @@ document.addEventListener('keydown', function (event) {
       break;
     case "d": // Move right
       camera.position.x -= speed;
-      modelTransform.rotateY = - Math.PI/2;
+      modelTransform.rotateY = -Math.PI / 2;
       modelTransform.translateX -= speed;
       break;
   }
 
   // Set the camera altitude to match the cube's altitude
   //camera.position.z = modelTransform.translateZ;
-  
+
   map.setFreeCameraOptions(camera);
 });
-
-
